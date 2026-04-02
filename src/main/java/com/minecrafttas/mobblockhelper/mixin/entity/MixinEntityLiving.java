098bcb7798ae1a8d8f80d45f74195a89e2bbde30
Noah Tung <uneasepurse37@gmail.com>
2026-04-02 16:05:01 -0700

    Modified MixinEntityLiving.java

--- src/main/java/com/minecrafttas/mobblockhelper/mixin/entity/MixinEntityLiving.java
+++ src/main/java/com/minecrafttas/mobblockhelper/mixin/entity/MixinEntityLiving.java
@@ -1,10 +1,13 @@
 package com.minecrafttas.mobblockhelper.mixin.entity;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import com.minecrafttas.mobblockhelper.MobBlockHelper;
 
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.item.ItemStack;
@@ -18,22 +21,34 @@ public class MixinEntityLiving {
 
 	/**
 	 * This sets the drop chances for the armor and hand slots for any mob to 100%
-	 * If you need to target the armor and hand separately you should copy this method and adjust using the
-	 * ordinal argument in the @At injection point
+	 * If you need to target the armor and hand separately you should copy this
+	 * method and adjust using the ordinal argument in the @At injection point
 	 */
 	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/util/Arrays;fill([FF)V"))
 	private void redirectArmorAndHandDropChances(float a[], float val, World world) {
-		Arrays.fill(a, 1F);
+
+		if (MobBlockHelper.isTASmodLoaded) {
+			Arrays.fill(a, 1F);
+		} else {
+			Arrays.fill(a, val);
+		}
 	}
-	
-	
+
 	/**
-	 * Changes the durability of an item when dropped by a mob.
-	 * Range can be anywhere from 1 to getMaxDamage -1 but here
-	 * I set it to give me 75% remaining just to be realistic
+	 * Changes the durability of an item when dropped by a mob. Range can be
+	 * anywhere from 1 to getMaxDamage -1 but here I set it to give me 75% remaining
+	 * just to be realistic
 	 */
 	@Redirect(method = "dropEquipment(ZI)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;setItemDamage(I)V"))
 	private void redirectItemDamage(ItemStack stack, int meta) {
-		stack.setItemDamage(((int) stack.getMaxDamage() * 1/4));
+
+		if (MobBlockHelper.isTASmodLoaded) {
+			stack.setItemDamage(((int) stack.getMaxDamage() * 1 / 4));
+		} else {
+			Random rand = new Random();
+
+			stack.setItemDamage(
+					stack.getMaxDamage() - rand.nextInt(1 + rand.nextInt(Math.max(stack.getMaxDamage() - 3, 1))));
+		}
 	}
 }
